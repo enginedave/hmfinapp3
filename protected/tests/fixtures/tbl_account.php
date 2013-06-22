54f841d0dcd1a6e91bb85463f7aca8856c97e3f7
Dave <davey.glenn@gmail.com>
2013-06-22 17:09:10 +0100

    Db test and account tests added

---
+++ protected/tests/fixtures/tbl_account.php
@@ -0,0 +1,34 @@
+<?php
+
+return array(
+	'account1'=>array(
+		'user_id'=>1,
+		'name'=>'account name one',
+		'type'=>'current',
+		'balance'=>'1234.99',
+		'create_time'=>'',
+		'create_user_id'=>'',
+		'update_time'=>'',
+		'update_user_id'=>'',
+	),
+	'account2'=>array(
+		'user_id'=>1,
+		'name'=>'account name two',
+		'type'=>'saving',
+		'balance'=>'786.99',
+		'create_time'=>'',
+		'create_user_id'=>'',
+		'update_time'=>'',
+		'update_user_id'=>'',
+	),
+	'account3'=>array(
+		'user_id'=>1,
+		'name'=>'account name three',
+		'type'=>'ISA',
+		'balance'=>'1534.09',
+		'create_time'=>'',
+		'create_user_id'=>'',
+		'update_time'=>'',
+		'update_user_id'=>'',
+	),
+);
---
+++ protected/tests/unit/AccountTest.php
@@ -0,0 +1,61 @@
+<?php
+class AccountTest extends CDbTestCase
+{
+	public $fixtures = array(
+		'accounts'=>'Account',
+	);
+
+	public function testCreate()
+	 {
+	 	//CREATE a new account
+	 	$newAccount=new Account;
+	 	$newAccountName = 'my test santander';
+	 	$newAccountType = 'one acc';
+	 	$newAccountBalance = '1234.45';
+	 	$newAccount->setAttributes(
+	 		array(
+	 			'user_id'=>1,
+	 			'name'=>$newAccountName,
+	 			'type'=>$newAccountType,
+	 			'balance'=>$newAccountBalance,
+	 			'create_time'=>'2013-03-15 00:00:00',
+	 			'create_user_id'=>1,
+	 			'update_time'=>'2013-03-15 00:00:00',
+	 			'update_user_id'=>1	 			
+	 		)
+	 	);
+	 	$this->assertTrue($newAccount->save(false));
+
+	 	//READ back the newly created account
+	 	$retreivedAccount = Account::model()->findByPk($newAccount->id);
+	 	$this->assertTrue($retreivedAccount instanceof Account);
+	 	$this->assertEquals($newAccountName, $retreivedAccount->name);
+
+	 }
+
+	 public function testRead(){
+	 	$retreivedAccount = $this->accounts('account1');
+	 	$this->assertTrue($retreivedAccount instanceof Account);
+	 	$this->assertEquals('account name one', $retreivedAccount->name);
+	 }
+
+	 public function testUpdate(){
+	 	$account = $this->accounts('account2'); //gets the second account from the database (fixture data)
+	 	//UPDATE account
+	 	$updatedAccountName = 'updated account name for one';
+	 	$account->name = $updatedAccountName;
+	 	$this->assertTrue($account->save(false));
+	 	//read by to test 
+	 	$updatedAccount = Account::model()->findByPk($account->id);
+	 	$this->assertEquals($updatedAccountName, $updatedAccount->name);
+	 }
+
+	 //delete not working as the databse wont delete the account as it violates an integrity - a referenced relation.
+	 public function testDelete(){
+	 	$account = $this->accounts('account3');//get the third set of fixture data from the database as it does not have an transactions in the fixture data as if it did it would cause a referential integrity error on deletion.
+	 	$savedAccountId = $account->id;
+	 	$this->assertTrue($account->delete());
+	 	$deletedAccount = Account::model()->findByPk($savedAccountId);
+	 	$this->assertEquals(NULL, $deletedAccount);
+	 }
+}
