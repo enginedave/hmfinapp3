48876017e872ca1f25284722971948e6c6fc2e29
Dave <davey.glenn@gmail.com>
2013-06-29 21:49:49 +0100

    Authentication against the database users table enabled

--- protected/components/UserIdentity.php
+++ protected/components/UserIdentity.php
@@ -1,33 +1,53 @@
 <?php
-
 /**
- * UserIdentity represents the data needed to identity a user.
- * It contains the authentication method that checks if the provided
- * data can identity the user.
- */
+* UserIdentity represents the data needed to identity a user.
+* It contains the authentication method that checks if the provided
+* data can identify the user.
+*/
 class UserIdentity extends CUserIdentity
 {
+	private $_id;
 	/**
-	 * Authenticates a user.
-	 * The example implementation makes sure if the username and password
-	 * are both 'demo'.
-	 * In practical applications, this should be changed to authenticate
-	 * against some persistent user identity storage (e.g. database).
-	 * @return boolean whether authentication succeeds.
-	 */
+	* Authenticates a user using the User data model.
+	* @return boolean whether authentication succeeds.
+	*/
 	public function authenticate()
 	{
-		$users=array(
-			// username => password
-			'demo'=>'demo',
-			'admin'=>'admin',
-		);
-		if(!isset($users[$this->username]))
+		$user=User::model()->findByAttributes(array('email'=>$this->username));
+		if($user===null)
+		{
 			$this->errorCode=self::ERROR_USERNAME_INVALID;
-		elseif($users[$this->username]!==$this->password)
-			$this->errorCode=self::ERROR_PASSWORD_INVALID;
+		}
 		else
-			$this->errorCode=self::ERROR_NONE;
+		{
+			if($user->password!==$user->encrypt($this->password))
+			
+			{
+				$this->errorCode=self::ERROR_PASSWORD_INVALID;
+			}
+			else
+			{
+				$this->_id = $user->id;
+				if(null===$user->last_login_time)
+				{
+					$lastLogin = time();
+				}
+				else
+				{
+					$lastLogin = strtotime($user->last_login_time);
+				}
+				$this->setState('lastLoginTime', $lastLogin); 
+				$this->errorCode=self::ERROR_NONE;
+			}
+		}
+		// If the errorCode is set to a non-zero integer i.e. there is an error
+		// then the following statement returns false. If the errorCode is 0 i.e. no errors 
+		// then the following statement returns true.
 		return !$this->errorCode;
 	}
+	public function getId()
+	{
+		return $this->_id;
+	}
 }
+
